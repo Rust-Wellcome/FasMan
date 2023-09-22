80af5070f72f7f521ce922b473cbcbc5dbdd468f
DLBPointon <damonlbp@hotmail.co.uk>
2023-09-22 10:42:33 +0100

    Update to structure, adding new function plans

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,48 @@ use std::env;
 use std::io::{BufReader, BufRead, Error};
 use clap::{Command, command, Arg};
 
-fn splitbycount() -> Result<(), std::io::Error> {
+
+fn splitbycount(file: &str, chunk: &u16) -> Result<(), std::io::Error> {
+    println!("hello");
+    //if bycount {
+    //    println!("Count to split by: {}", bycount.unwrap().get_one::<String>("count").unwrap());
+   // }
+    // returns bool bycount.unwrap().contains_id("count");
+    
+    // ---
+    let  chunk_val = chunk.clone();
+    let mut counter = 0;
+    let mut global_counter = 0;
+
+    //let mut output = File::create(path)?;
+    //write!(output, "Rust\n💖\nFun")?;
+
+    let input = File::open(file)?;
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        if counter != chunk_val {
+            if line?.starts_with('>') {
+                println!("header");
+            } else {
+                println!("Sequence");
+                counter += 1;
+            }
+        } else {
+            counter = 0;
+            println!("CHUNK");
+        }
+    }
+    Ok(())
+}
+
+fn splitbysize() -> Result<(), std::io::Error> {
     println!("hello");
 
     Ok(())
 }
 
-fn splitbysize() -> Result<(), std::io::Error> {
+fn mapheaders() -> Result<(), std::io::Error> {
     println!("hello");
 
     Ok(())
@@ -33,6 +68,7 @@ fn main() -> Result<(), Error> {
                     .short('c')
                     .long("file-count")
                     .aliases(["count"])
+                    .value_parser(clap::value_parser!(u16))
                     .help("How many sequences per file")
             )
     )
@@ -51,7 +87,27 @@ fn main() -> Result<(), Error> {
                     .short('s')
                     .long("mem-size")
                     .required(true)
+                    .value_parser(clap::value_parser!(u16))
                     .help("Size in MB that a fasta file is to be chunked into")
+            )
+    )
+    .subcommand(
+        Command::new("mapheaders")
+            .about("Command for stripping out headers and replacing with a standardised automatic or user-given string, this also returns a dict of old:new headers")
+            .arg(
+                Arg::new("fasta-file")
+                    .short('f')
+                    .aliases(["fasta"])
+                    .required(true)
+                    .help("A path to a valid fasta file.")
+            )
+            .arg(
+                Arg::new("replace-with")
+                    .short('r')
+                    .aliases(["replacement"])
+                    .required(false)
+                    .default_value("FMMH")
+                    .help("The new header format, appended with a numerical value. Without being set the new header will default to 'FM_{numberical}'")
             )
     )
     .get_matches();
@@ -61,45 +117,24 @@ fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
 
     if &args[1].to_string() == "splitbycount" {
-        let bycount = match_result.subcommand_matches("splitbycount");
-        println!("Fasta file for processing: {:?}", bycount.unwrap().get_one::<String>("fasta-file").unwrap());
-        println!("Number of sequence-header pairs per file: {:?}", bycount.unwrap().get_one::<String>("count").unwrap());
-        splitbycount();
+        let arguments = match_result.subcommand_matches("splitbycount");
+        let fasta_file = arguments.unwrap().get_one::<String>("fasta-file").unwrap();
+        let fasta_count = arguments.unwrap().get_one::<u16>("count").unwrap();
+        println!("Fasta file for processing: {:?}", fasta_file);
+        println!("{:?}", &fasta_count);
+        println!("Number of sequence-header pairs per file: {:?}", fasta_count);
+        splitbycount(fasta_file, &fasta_count);
     } else if &args[1].to_string() == "splitbysize" {
-        let bysize = match_result.subcommand_matches("splitbysize");
-        println!("Fasta file for processing: {:?}", bysize.unwrap().get_one::<String>("fasta-file").unwrap());
-        println!("Size to chunk fasta into: {:?}", bysize);
+        let arguments = match_result.subcommand_matches("splitbysize");
+        println!("Fasta file for processing: {:?}", arguments.unwrap().get_one::<String>("fasta-file").unwrap());
+        println!("Size to chunk fasta into: {:?}", arguments.unwrap().get_one::<u16>("mem-size").unwrap());
         splitbysize();
+    } else if &args[1].to_string() == "mapheaders" {
+        let arguments = match_result.subcommand_matches("mapheaders");
+        println!("Fasta file for processing: {:?}", arguments.unwrap().get_one::<String>("fasta-file").unwrap());
+        println!("Replace headers with string: {:?}", arguments.unwrap().get_one::<String>("replace-with").unwrap());
+        mapheaders();
     };
-    //if bycount {
-    //    println!("Count to split by: {}", bycount.unwrap().get_one::<String>("count").unwrap());
-   // }
-    // returns bool bycount.unwrap().contains_id("count");
-    
-    // ---
-    let path = "test.fasta";
-    let chunk = 2;
-    let mut counter = 0;
-
-    //let mut output = File::create(path)?;
-    //write!(output, "Rust\n💖\nFun")?;
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    for line in buffered.lines() {
-        if counter != chunk {
-            if line?.starts_with('>') {
-                println!("header");
-            } else {
-                println!("Sequence");
-                counter += 1;
-            }
-        } else {
-            counter = 0;
-            println!("CHUNK");
-        }
-    }
 
     Ok(())
 }
