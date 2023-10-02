b8a659d65e55e9d8b4fb25079e288eda9ec07dde
DLBPointon <damonlbp@hotmail.co.uk>
2023-10-02 22:54:27 +0100

    Updating

---
+++ src/mapheaders.rs
@@ -0,0 +1,8 @@
+pub mod mapheaders {
+    fn mapfastahead(file: &str, _sep: &str) -> Result<(), std::io::Error> {
+        println!("Mapping headers for file: {}", file);
+    
+        Ok(())
+    }
+    
+}
