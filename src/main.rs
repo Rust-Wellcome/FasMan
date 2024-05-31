b0d7592bf23f00f6f96519263518a1a6ea4820ed
Dasun Pubudumal <pubudumald@gmail.com>
2024-05-31 16:03:13 +0100

    Refactoring main function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 #![allow(non_snake_case)]
 
-use std::io::Error;
-
 use fasta_manipulation::run;
 
 // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#separation-of-concerns-for-binary-projects
-fn main() -> Result<(), Error> {
+fn main() {
     if let Err(e) = run() {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     } else {
-        Ok(())
+        println!("Done!");
     }
 }
