9b5caadb668e039b33bd7dd0f0778b3522bfbc93
Dasun Pubudumal <pubudumald@gmail.com>
2024-06-04 18:19:16 +0100

    Setup human-panic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![allow(non_snake_case)]
 
+use human_panic::setup_panic;
 use fasta_manipulation::run;
 
 // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#separation-of-concerns-for-binary-projects
 fn main() {
+    // https://rust-cli.github.io/book/in-depth/human-communication.html
+    setup_panic!();
     if let Err(e) = run() {
         eprintln!("Error: {}", e);
         std::process::exit(1);
