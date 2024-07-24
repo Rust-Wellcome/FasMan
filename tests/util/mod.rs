318caa5dc57594021d8c6e8fa870459b74f31b96
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-07-24 09:44:27 +0100

    chore: Add pattern matching to extract bool result in are_files_identical function

--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -3,6 +3,7 @@ use std::{fs, io::ErrorKind};
 /// Checks whether the contents of the two files are identical.
 /// file_path1 and file_path2 are input file paths.
 /// Returns erred Result struct for errors.
+/// Use pattern matching to extract the resulting bool.
 pub fn are_files_identical(file_path1: &str, file_path2: &str) -> std::io::Result<bool> {
     match (fs::read(file_path1), fs::read(file_path2)) {
         (Ok(contents1), Ok(contents2)) => Ok(contents1 == contents2),
