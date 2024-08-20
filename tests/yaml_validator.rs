3478b52d4c4214804d427a9b90a6040c161456d1
Stephen <519327+stevieing@users.noreply.github.com>
2024-08-20 15:00:23 +0100

    chore: yaml_validator fix linting issues

--- tests/yaml_validator.rs
+++ tests/yaml_validator.rs
@@ -1,4 +1,4 @@
-use fasta_manipulation::yaml_validator_mod::{ validate_paths, get_file_list, YamlResults};
+use fasta_manipulation::yaml_validator_mod::{get_file_list, validate_paths, YamlResults};
 use std::path::PathBuf;
 
 #[test]
@@ -9,20 +9,21 @@ fn check_validate_paths() {
 
 #[test]
 fn check_get_file_list() {
-  let path = "test_data/iyAndFlav1".to_string();
-  let expected_file_list: Vec<PathBuf> = vec![
-    PathBuf::from("test_data/iyAndFlav1/full/iyAndFlav1_subset.fa.fai"), 
-    PathBuf::from("test_data/iyAndFlav1/full/iyAndFlav1_subset.fa"), 
-  PathBuf::from("test_data/iyAndFlav1/full/iyAndFlav1.curated_subset.tpf"), 
-  PathBuf::from("test_data/iyAndFlav1/small/small_test.fa.fai"), 
-  PathBuf::from("test_data/iyAndFlav1/small/small_test.fa"), 
-  PathBuf::from("test_data/iyAndFlav1/small/small_test.curated.tpf"), 
-  PathBuf::from("test_data/iyAndFlav1/small/small_test.output.fasta"), 
-  PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.curated.tpf"), 
-  PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.output.fasta"), 
-  PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.fa"), 
-  PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.fa.fai"),
-  PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.debug.txt")];
-  let file_list = get_file_list(&path);
-  assert_eq!(expected_file_list, file_list);
+    let path = "test_data/iyAndFlav1".to_string();
+    let expected_file_list: Vec<PathBuf> = vec![
+        PathBuf::from("test_data/iyAndFlav1/full/iyAndFlav1_subset.fa.fai"),
+        PathBuf::from("test_data/iyAndFlav1/full/iyAndFlav1_subset.fa"),
+        PathBuf::from("test_data/iyAndFlav1/full/iyAndFlav1.curated_subset.tpf"),
+        PathBuf::from("test_data/iyAndFlav1/small/small_test.fa.fai"),
+        PathBuf::from("test_data/iyAndFlav1/small/small_test.fa"),
+        PathBuf::from("test_data/iyAndFlav1/small/small_test.curated.tpf"),
+        PathBuf::from("test_data/iyAndFlav1/small/small_test.output.fasta"),
+        PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.curated.tpf"),
+        PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.output.fasta"),
+        PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.fa"),
+        PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.fa.fai"),
+        PathBuf::from("test_data/iyAndFlav1/tiny/tiny_test.debug.txt"),
+    ];
+    let file_list = get_file_list(&path);
+    assert_eq!(expected_file_list, file_list);
 }
